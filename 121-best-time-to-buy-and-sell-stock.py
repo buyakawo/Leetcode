d2559e9fb29ce38526c9c1a732fffc60a6ced698
buyakawo <wissalelamoury@gmail.com>
2026-03-05 14:29:16 +0000

    prob 121

--- 121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock.py
@@ -4,3 +4,17 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
+        if not prices:
+            return 0
+
+        buy=prices[0]
+        profit=0
+
+        for i in range(1, len(prices)):
+            if prices[i] < buy:
+                buy=prices[i]
+            if prices[i]-buy > profit:
+                profit=prices[i]-buy
+
+        return profit
+
