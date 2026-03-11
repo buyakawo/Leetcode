d38b2a250879007b8bbb69144ba8d90f3e31aa87
buyakawo <wissalelamoury@gmail.com>
2026-03-11 22:34:28 +0000

    prob 35

---
+++ 35-search-insert-position.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+
+        for i in range(0, len(nums)):
+            if nums[i] >= target:
+                return i
+
+        return i+1
